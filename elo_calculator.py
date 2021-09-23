29e3510871fae0d84130b142422c3257956dc4ac
dsszymanski <dan1993us@yahoo.com>
2021-09-22 20:13:52 -0400

    Restructured fns to take an args list as input instead of individual inputs

--- elo_calculator.py
+++ elo_calculator.py
@@ -1,18 +1,29 @@
-K = 20
+HOME_WIN = 0
+HOME_LOSS = 1
+HOME_DRAW = 2
 
 class EloCalculator:
     @staticmethod
-    def basic_elo_change(t1_elo, t2_elo, winner):
-        t1_expected = EloCalculator.basic_expected(t1_elo, t2_elo)
-        t2_expected = 1 - t1_expected
+    def basic_elo_change(home_elo, away_elo, result, args):
+        K = args['K'] if 'K' in args.keys() else 32
+        home_expected = EloCalculator.basic_expected(home_elo, away_elo, args)
+        away_expected = 1 - home_expected
         
-        t1_change = K * ((1 if winner == 0 else 0) - t1_expected)
-        t2_change = K * ((1 if winner == 1 else 0) - t2_expected)
+        if result == HOME_DRAW:
+            home_change = K * (0.5 - home_expected)
+            away_change = K * (0.5 - away_expected)
+        elif result == HOME_WIN:
+            home_change = K * (1 - home_expected)
+            away_change = K * (0 - away_expected)
+        else: #HOME_LOSS
+            home_change = K * (0 - home_expected)
+            away_change = K * (1 - away_expected)
         
-        return [t1_change, t2_change]
+        return {'Home Change': home_change, 'Away Change': away_change}
 
     @staticmethod
-    def basic_expected(t1_elo, t2_elo):
-        exp = (t2_elo - t1_elo) / 400
+    def basic_expected(t1_elo, t2_elo, args):
+        rating_factor = args['rating_factor'] if 'rating_factor' in args.keys() else 400
+        exp = (t2_elo - t1_elo) / rating_factor
         base = 1 + 10 ** exp
         return 1 / base
