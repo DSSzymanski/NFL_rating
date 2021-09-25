756cf39baa685453f7b49a3375176a34f6900914
dsszymanski <dan1993us@yahoo.com>
2021-09-25 01:34:00 -0400

    Changed name and converted calc_win_loss_stats to new class dictionary from old ways.

--- stat_eval.py
+++ stat_eval.py
@@ -4,37 +4,33 @@ import elo_calculator as ec
 def calc_win_loss_stats():
     #init data
     team_data = DataHandler.get_teams_file_data()
-    team_id_dict = DataHandler.team_name_to_id_dict(team_data)
+    teams = DataHandler.get_teams(team_data)
     games = DataHandler.get_games_file_data()
-    team_stats = DataHandler.elo_dict(team_id_dict)
-    
-    #change to record team_statss instead of elo    
-    for team in team_stats.keys():
-        team_stats[team] = {'Wins': 0, 'Losses': 0, 'Draws': 0}
-    
+
     for game in games:
         #get team ids for both teams
-        team_home = team_id_dict[game['team_home']]
-        team_away = team_id_dict[game['team_away']]
+        team_home = teams[game['team_home']]
+        team_away = teams[game['team_away']]
         
         #calc if home win, draw, or home loss
         if game['score_home'] == game['score_away']:
-            team_stats[team_home]['Draws'] += 1
-            team_stats[team_away]['Draws'] += 1
+            team_home.add_draw()
+            team_away.add_draw()
         elif game['score_home'] > game['score_away']:
-            team_stats[team_home]['Wins'] += 1
-            team_stats[team_away]['Losses'] += 1
+            team_home.add_win()
+            team_away.add_loss()
         else:
-            team_stats[team_home]['Losses'] += 1
-            team_stats[team_away]['Wins'] += 1
+            team_home.add_loss()
+            team_away.add_win()
 
-    for team_id, results in team_stats.items():
-        print(f"{team_id}\t\tWins: {results['Wins']}\t\tLosses: {results['Losses']}\t\tDraws: {results['Draws']}")
+    
+    for team in sorted(set(teams.values())):
+        print(team)
     
     sum_games = len(games)
-    sum_draws = sum([team['Draws'] for team in team_stats.values()])
+    sum_draws = int(sum([team.get_draws() for team in set(teams.values())]) / 2)
     print(f"Total Games: {sum_games}\t\tTotal Draws: {sum_draws}")
-
+    
 #calc elo using inputed fn
 def calc_end_elo(eval_fn, args):
     team_data = DataHandler.get_teams_file_data()
@@ -152,3 +148,4 @@ def prediction_expanded_stats(K=32, rating_factor=400, hfa_val=50):
     percent = result_stats["Right"] / (result_stats["Right"] + result_stats["Wrong"])
     
     return [result_stats, percent]
+calc_win_loss_stats()
