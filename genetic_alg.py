90a4b048a70baf2271f382951e1f1ddf313eeccc
dsszymanski <dan1993us@yahoo.com>
2021-09-28 23:50:57 -0400

    Added to genetic algorithm.

--- genetic_alg.py
+++ genetic_alg.py
@@ -3,14 +3,17 @@ from stat_eval import prediction_expanded_stats
 import math
 
 def genetic_algorithm(pop_size, generations=1):
+    checked = {}
+    checked['saved'] = 0
     population = initial_population(pop_size)
-    pop_fitness = population_fitnesses(population)[:2*math.floor(pop_size/3)]
+    pop_fitness = population_fitnesses(population, checked)[:2*math.floor(pop_size/3)]
+    pop_fitness[0][1] = round(pop_fitness[0][1], 5)
+    pop_fitness[len(pop_fitness)-1][1] = round(pop_fitness[len(pop_fitness)-1][1], 5)
     print(f'Initial Generation: Best: {pop_fitness[0]} Worst: {pop_fitness[len(pop_fitness)-1]}')
+    
     for gen in range(1, generations+1):
         new_population = []
-        
         new_population.append(pop_fitness[0][0])
-        
         
         for i in range(1, pop_size):
             parent1 = pop_fitness[randint(0, len(pop_fitness)-1)][0]
@@ -19,19 +22,22 @@ def genetic_algorithm(pop_size, generations=1):
             new_population.append(child)
             
         population = new_population
-        pop_fitness = population_fitnesses(population)[:2*math.floor(pop_size/3)]
+        pop_fitness = population_fitnesses(population, checked)[:2*math.floor(pop_size/3)]
+        pop_fitness[0][1] = round(pop_fitness[0][1], 5)
+        pop_fitness[len(pop_fitness)-1][1] = round(pop_fitness[len(pop_fitness)-1][1], 5)
         print(f'Generation {gen}: Best: {pop_fitness[0]} Worst: {pop_fitness[len(pop_fitness)-1]}')
+        print(checked['saved'], len(checked.keys()))
         
     return pop_fitness
     
 def reproduce(p1, p2):
     child = []
+    #print(p1, p2)
     for gene in range(len(p1)):
         prob = random()
-        
-        if prob <= .45:
+        if prob <= .4:
             child.append(p1[gene])
-        elif prob <= .9:
+        elif prob <= .8:
             child.append(p2[gene])
         else:
             child.append(mutate(gene))
@@ -42,23 +48,44 @@ def mutate(index):
         return randomK()
     elif index == 1:
         return randomRF()
-    else:
+    elif index == 2:
         return randomHFA()
+    else:
+        return randomScale()
 
-def population_fitnesses(pop):
+def population_fitness(single, checked):
+    if str(single) not in checked:
+        prediction = prediction_expanded_stats(single[0], single[1], single[2], single[3])[1]
+        checked[str(single)] = prediction
+        return [single, prediction]
+    else:
+        checked['saved'] += 1
+        return [single, checked[str(single)]]
+
+def population_fitnesses(pop, checked):
      pop_fitness = []
      for single in pop:
-         pop_fitness.append([single, prediction_expanded_stats(single[0], single[1], single[2])[1]])
+         if str(single) not in checked:
+             prediction = prediction_expanded_stats(single[0], single[1], single[2], single[3])[1]
+             checked[str(single)] = prediction
+             pop_fitness.append([single, prediction])
+         else:
+             checked['saved'] += 1
+             pop_fitness.append([single, checked[str(single)]])
      return sorted(pop_fitness, key=lambda x:x[1], reverse=True)
 
 def initial_population(pop_size):
     pop = []
     for x in range(pop_size):
-        K = randomK()
-        rating_factor = randomRF()
-        hfa = randomHFA()
-        pop.append([K, rating_factor, hfa])
+        pop.append(create_single())
     return pop
+
+def create_single():
+    K = randomK()
+    rating_factor = randomRF()
+    hfa = randomHFA()
+    scale = randomScale()
+    return [K, rating_factor, hfa, scale]
 
 def randomK():
     return randint(0, 1000)
@@ -69,4 +96,7 @@ def randomRF():
 def randomHFA():
     return randint(0, 500)
 
-genetic_algorithm(50, 25)
+def randomScale():
+    return randint(0, 100)/100
+
+genetic_algorithm(250, 100)
