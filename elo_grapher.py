7c0d751739d3bd8f9920f7a10bdd84f05e914d16
dsszymanski <dan1993us@yahoo.com>
2021-09-30 23:48:14 -0400

    Added tenative working function that plots multiple team's elo for comparison.

--- elo_grapher.py
+++ elo_grapher.py
@@ -9,3 +9,19 @@ def graph_single_team(team_name):
     ax.set_xlabel('Date')
     ax.set_ylabel('Elo')
     ax.set_title(f"{team_name}'s elo over time")
+
+def graph_multiple_teams(team_names):
+    team_data = prediction_basic_stats()[1]
+    fig, ax = plt.subplots()
+    
+    for team in team_names:
+        data = team_data[team].get_graph_data()
+        ax.plot(data[0], data[1], label=team)
+    
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Elo')
+    ax.legend(loc='upper center', bbox_to_anchor=(.5, 1.2), fontsize='small', ncol=2)
+#eams = ['Kansas City Chiefs', 'Las Vegas Raiders', 'Miami Dolphins', 'New England Patriots']
+#graph_multiple_teams(teams)
+
+print(team_data = prediction_basic_stats()[1])
