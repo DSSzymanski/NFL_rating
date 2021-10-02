fffc0e4f38384f17fd9601372718deeded01e68f
dsszymanski <dan1993us@yahoo.com>
2021-10-02 02:08:49 -0400

    Added more graph axis design. Moved axis design for both graphing functions to another function. Documented file.

--- elo_grapher.py
+++ elo_grapher.py
@@ -1,5 +1,40 @@
+"""
+The elo_grapher module controls creating graphs of nfl_team's elo over time using
+matplotlib. The module contains 2 main functions, graph_single_team, which displays
+a single nfl_team object's elo history over time, and graph_multiple_teams, which
+displays up to 4 nfl_teams elo history over time.
+
+Usage
+-----
+graph_single_team takes in a single team_name and runs through all games calculating
+each nfl_team's elo. Then after, it graphs the single nfl_team that's assigned
+to the team_name on a graph.
+
+graph_mutliple_teams takes in a list of team_names and runs through all games
+calculating each nfl_team's elo. Then after, it takes the nfl_team objects assigned
+to each team_name in the list and graphs them. LIMIT: 4 teams.
+
+NOTE: the functions require a team_name, not a team_id, and teams that have had
+multiple team_names, e.g. "Las Vegas Raiders", "Los Angeles Raiders", and "Oakland
+Raiders" are all associated with the same nfl_team and will return the same graph.
+
+Methods
+-------
+graph_single_team(strteam_name):
+    graphs a single nfl_team's elo_history on a graph.
+graph_single_team(list[str] team_names):
+    graphs multiple nfl_teams' elo_historys on a graph.
+_setup_axes(axes plt.axes):
+    sets up common attributes used for multiple graphs.
+_get_elo_max_min(dict{str team_name : nfl_team} data) -> namedtuple:
+    iterates over every unique nfl_team in the data dict values and returns
+    the max and min elo values in a namedtuple (tuple.Max, tuple.Min).
+"""
+
 from collections import namedtuple
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.ticker import MultipleLocator
 from stat_eval import prediction_basic_stats
 
 def graph_single_team(team_name):
@@ -13,13 +48,19 @@ def graph_single_team(team_name):
         Team name input for which team you want graphed.
         NOTE: needs team_name, not team_id for a string.
     """
-    team_data = prediction_basic_stats()[1][team_name].get_graph_data()
-    fig, ax = plt.subplots()
+    data = prediction_basic_stats()[1]
+    bounds = _get_graph_max_min(data)
+    team_data = data[team_name].get_graph_data()
+
+    axes = plt.subplots()[1]
     #team_data = [team.date_history, team.elo_history]
-    ax.plot(team_data[0], team_data[1])
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Elo')
-    ax.set_title(f"{team_name}'s elo over time")
+    axes.plot(team_data[0], team_data[1])
+
+    plt.ylim(bounds.Min, bounds.Max)
+
+    _setup_axis(axes)
+
+    axes.set_title(f"{team_name}'s elo over time")
 
 def graph_multiple_teams(team_names):
     """
@@ -43,21 +84,47 @@ def graph_multiple_teams(team_names):
         return 'Too many teams'
 
     team_data = prediction_basic_stats()[1]
-    fig, ax = plt.subplots()
+    bounds = _get_graph_max_min(team_data)
+    axes = plt.subplots()[1]
 
     for team in team_names:
         #data = [team.date_history, team.elo_history]
         data = team_data[team].get_graph_data()
-        ax.plot(data[0], data[1], label=team)
+        axes.plot(data[0], data[1], label=team)
 
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Elo')
-    ax.grid(b=True, which='major') #setup major gridlines
+    plt.ylim(bounds.Min, bounds.Max)
+
+    _setup_axis(axes)
+
     #move legend off plot for clarity
-    ax.legend(loc='upper center', bbox_to_anchor=(.5, 1.2), fontsize='small', ncol=2)
+    axes.legend(loc='upper center', bbox_to_anchor=(.5, 1.2), fontsize='small', ncol=2)
     return 'Graphing completed'
 
-def get_graph_max_min(data):
+def _setup_axis(axes):
+    """
+    Sets up common attributes on axes for multiple graphing functions that share
+    the same setup.
+
+    Parameters
+    ----------
+    axes : pyplot plt.subplot
+        subplot to setup common attr. on.
+
+    Returns
+    -------
+    None.
+
+    """
+    axes.set_xlabel('Date')
+    axes.set_ylabel('Elo')
+
+    axes.grid(b=True, which='major') #setup major gridlines
+    axes.grid(b=True, which='minor', axis='y', linestyle='--')
+
+    axes.xaxis.set_minor_locator(mdates.MonthLocator(interval=12))
+    axes.yaxis.set_minor_locator(MultipleLocator(50))
+
+def _get_graph_max_min(data):
     """
     Searches data to find the lowest elo point and the highest elo point across
     all teams to fix a graphs's y-axis.
@@ -76,13 +143,11 @@ def get_graph_max_min(data):
         Call namedtuple.Max, namedtuple.Min.
     """
     Bounds = namedtuple('Bounds', 'Max, Min')
-    Max, Min = None, None
+    elo_max, elo_min = None, None
     for team in set(data.values()):
         elos = team.get_graph_data()[1]
-        if Max is None or max(elos) > Max:
-            Max = max(elos)
-        if Min is None or min(elos) < Min:
-            Min = min(elos)
-    return Bounds(Max=Max, Min=Min)
-teams = ['Kansas City Chiefs', 'Las Vegas Raiders', 'Miami Dolphins', 'New England Patriots']
-graph_multiple_teams(teams)
+        if elo_max is None or max(elos) > elo_max:
+            elo_max = max(elos)
+        if elo_min is None or min(elos) < elo_min:
+            elo_min = min(elos)
+    return Bounds(Max=elo_max, Min=elo_min)
