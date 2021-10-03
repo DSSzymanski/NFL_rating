53d1fc40c8b180a574ad59d650e5866bc2e9312c
dsszymanski <dan1993us@yahoo.com>
2021-10-03 01:44:08 -0400

    Documented file. Changed prediction_basic_stats to record result in game['result'] in if/ 	else loop instead of result var like in expanded. Changed get_win_rate to get_accuracy for clairty (it calculated % of right 	predictions, not wins).

--- stat_eval.py
+++ stat_eval.py
@@ -1,3 +1,35 @@
+"""
+The stat_eval module is responsible for getting statistics based on the
+GAMES_FILE data. Most functions incorporate using the elo_calculator functions
+to get prediction data for the different games.
+
+Methods
+-------
+calc_win_loss() -> none:
+    function that runs through all the games and adds up each team's wins, losses
+    and draws. At the end of the function, prints all the nfl_teams to the console
+    and then prints the total number of games and total number of draws.
+calc_end_elo(fn eval_fn) -> none:
+    iterates all the games and calculates each team's elo changes based on the
+    results. Once completed, prints all the nfl_teams to console.
+prediction_basic_stats() -> list[dict, dict]:
+    iterates through all the games and predicts winners based on the teams elo,
+    then compares the end result to the prediction. The function monitors the num
+    of right/wrong predictions and updates each nfl_team's elo after each game
+    using the basic standard elo calculation. Once completed, returns a dict of
+    right/wrong predictions and the teams dict with updated nfl_teams.
+prediction_expanded_stats(int, int, int, float, float) -> list[dict, float]:
+    iterates through all the games and predicts winners based on the teams elo,
+    then compares the end result to the prediction. The function monitors the num
+    of right/wrong predictions and updates each nfl_team's elo after each game
+    using the expanded elo calculation with the input parameters. Once
+    completed, returns a dict of right/wrong predictions and a float with the
+    right prediction percentage.
+get_accuracy(dict stats) -> float:
+    takes in a dict of 'Right' and 'Wrong' values and returns the percentage of
+    right predictions.
+"""
+
 from data_handler import DataHandler
 from elo_calculator import EloCalculator as ec
 from elo_calculator import HOME_WIN, HOME_LOSS, HOME_DRAW
@@ -41,6 +73,23 @@ def calc_win_loss():
 
 #calc elo using inputed fn
 def calc_end_elo(eval_fn):
+    """
+    Calc_end_elo runs all games from the GAMES_FILE data and only calculates the
+    elo from the results with no prediction modeling. After the games are ran,
+    prints all nfl_team objects to the console.
+
+    Parameters
+    ----------
+    eval_fn : function
+        selection of which elo evaluation function to use (ec.basic_elo_change
+        or ec.expanded_elo_change).
+
+    Returns
+    -------
+    None
+        Prints the str value of each nfl_team object to console.
+
+    """
     teams = DataHandler.get_teams()
     games = DataHandler.get_games_file_data()
 
@@ -51,16 +100,16 @@ def calc_end_elo(eval_fn):
 
         #calc if home win, draw, or home loss
         if game['score_home'] == game['score_away']:
-            team_home.add_draw()
-            team_away.add_draw()
+            team_home.add_draw() #for __str__
+            team_away.add_draw() #for __str__
             result = HOME_DRAW
         elif game['score_home'] > game['score_away']:
-            team_home.add_win()
-            team_away.add_loss()
+            team_home.add_win() #for __str__
+            team_away.add_loss() #for __str__
             result = HOME_WIN
         else:
-            team_home.add_loss()
-            team_away.add_win()
+            team_home.add_loss() #for __str__
+            team_away.add_win() #for __str__
             result = HOME_LOSS
 
         changes = eval_fn(team_home.elo, team_away.elo, result)
@@ -73,6 +122,19 @@ def calc_end_elo(eval_fn):
         print(team)
 
 def prediction_basic_stats():
+    """
+    Function for predicting all games with standard elo calculations.
+
+    Function runs through all games imported from the games file and predicts
+    the winner then records if the prediction was right or not. The fn then
+    returns the results at the end.
+
+    Returns
+    -------
+    list
+        returns list of 2 items. First is a dict of 'Right'ly predicted games and
+        'Wrong'ly predicted games. Second is the dict of 'team_name's to nfl_teams.
+    """
     teams = DataHandler.get_teams()
     games = DataHandler.get_games_file_data()
     result_stats = {"Right": 0, "Wrong": 0}
@@ -87,21 +149,20 @@ def prediction_basic_stats():
 
         #calc if home win, draw, or home loss
         if game['score_home'] == game['score_away']:
-            result = HOME_DRAW
+            game['Result'] = HOME_DRAW
             team_home.add_draw()
             team_away.add_draw()
         elif game['score_home'] > game['score_away']:
-            result = HOME_WIN
+            game['Result'] = HOME_WIN
             team_home.add_win()
             team_away.add_loss()
         else:
-            result = HOME_LOSS
+            game['Result'] = HOME_LOSS
             team_home.add_loss()
             team_away.add_win()
 
-        game['Result'] = result
-
-        changes = ec.basic_elo_change(team_home.get_elo(), team_away.get_elo(), result)
+        changes = ec.basic_elo_change(team_home.get_elo(), team_away.get_elo(),\
+                                      game['Result'])
         team_home.inc_elo(changes['Home Change'])
         team_away.inc_elo(changes['Away Change'])
 
@@ -115,8 +176,40 @@ def prediction_basic_stats():
 
     return [result_stats, teams]
 
-def prediction_expanded_stats(k=32, rating_factor=400, hfa_val=50,\
+def prediction_expanded_stats(k=32, rating_factor=400, hfa_val=0,\
                                          season_scale=1, playoff_multiplier=1):
+    """
+    Prediction expanded stats is the function that is based off elo, but with
+    additional parameters to try to find the model where the teams' elo most
+    accurately predicts the winner of a game. If no parameters are supplied,
+    it will run the standard elo calculation with the default values.
+
+    Function runs through all games imported from the games file and predicts
+    the winner then records if the prediction was right or not. The fn then
+    returns the results at the end.
+
+    Parameters
+    ----------
+    k : int, optional
+        k-factor value for elo calc. The default is 32.
+    rating_factor : int, optional
+         rating-factor value for elo calc. The default is 400.
+    hfa_val : int, optional
+        home field advantage value for elo calc. The default is 0.
+    season_scale : float, optional
+        float to multiply each elo by in between seasons. The default is 1.
+    playoff_multiplier : float, optional
+        float multiplier applied to elos if the game is a playoff game. The
+        default is 1.
+
+    Returns
+    -------
+    list
+        returns list of 2 items. First is a dict of 'Right'ly predicted games and
+        'Wrong'ly predicted games. Second is the win rate percentage associated
+        with the predicted games using the inputed params.
+
+    """
     teams = DataHandler.get_teams()
     games = DataHandler.get_games_file_data()
     result_stats = {"Right": 0, "Wrong": 0}
@@ -130,8 +223,10 @@ def prediction_expanded_stats(k=32, rating_factor=400, hfa_val=50,\
         for team in [team_home, team_away]:
             team.adj_season(game['schedule_season'], season_scale)
 
+        #if the stadium benefits the home team take value
         hfa = hfa_val if game['stadium_neutral'] == "FALSE" else 0
 
+        #calculate predicted winner
         if ec.expanded_expected(team_home.get_elo(), team_away.get_elo(), rating_factor, hfa) >= .5:
             game['Prediction'] = HOME_WIN
         else:
@@ -155,11 +250,11 @@ def prediction_expanded_stats(k=32, rating_factor=400, hfa_val=50,\
         else:
             result_stats["Wrong"] += 1
 
-    return [result_stats, _get_win_rate(result_stats)]
+    return [result_stats, get_accuracy(result_stats)]
 
-def _get_win_rate(stats):
+def get_accuracy(stats):
     """
-    Calculates and returns the win rate percentage.
+    Calculates and returns the percentage of games predicted right.
 
     Parameters
     ----------
